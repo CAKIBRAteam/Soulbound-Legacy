f1d2125ddf1902c10b8dc62b24d807717be2b30e
imnotgoose <ryuminstepan@yandex.ru>
2025-06-13 01:35:50 +0300

    Delete startup_scripts directory

--- startup_scripts/Soulbound_legacy/items/custom items/default/black_pearl.js
+++
@@ -1,4 +0,0 @@
-StartupEvents.registry('item', event => {
-	event.create('black_pearl')
-	.texture('kubejs:item/black_pearl')
-})
--- startup_scripts/Soulbound_legacy/items/custom items/icons/chapter_6_icon.js
+++
@@ -1,3 +0,0 @@
-StartupEvents.registry('item', event => {
-	event.create('chapter.6.icon').texture('kubejs:item/chapter_6_icon')
-})
