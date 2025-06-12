3a6b873a197327927e137075aebc516f7a7c6166
imnotgoose <ryuminstepan@yandex.ru>
2025-06-13 01:35:17 +0300

    Delete client_scripts directory

--- client_scripts/example.js
+++
@@ -1,8 +0,0 @@
-// priority: 0
-
-// Visit the wiki for more info - https://kubejs.com/
-
-console.info('Hello, World! (Loaded client scripts)')
-NetworkEvents.dataReceived("close_gui", event => {
-	Client.setScreen(null)
-})
